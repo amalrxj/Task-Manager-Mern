f44371fd252a8fcb3a3ccb4dc49b19e4f64f66d7
Amal Raj <amalrajkvdev@gmail.com>
2025-03-30 17:21:43 +0530

    input page design

---
+++ frontend/Task-Manager/src/components/layouts/AuthLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const AuthLayout = ({ children }) => {
+  return (
+    <div className="flex">
+      <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12 ">
+        <h2 className="text-lg font-medium text-black">Task Manager</h2>
+        {children}
+      </div>
+      <div className="hidden md:flex w-[40vw] h-screen items-center bg-blue-50 bg-[url('/bgimg.jpg')] bg-cover bg-no-repeat bg-center overflow-hidden p-8">
+        {/* <img src="" alt="ui-img" className="" /> */}
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
