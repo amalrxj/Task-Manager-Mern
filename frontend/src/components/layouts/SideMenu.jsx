91dcb1e2ebac5455f88600e8843fb87cab94434e
Amal Raj <amalrajkvdev@gmail.com>
2025-04-16 12:37:26 +0530

    update and fix the errors in img url

--- frontend/src/components/layouts/SideMenu.jsx
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -37,7 +37,7 @@ const SideMenu = ({ activeMenu }) => {
       <div className="flex flex-col items-center justify-center pt-5 mb-7">
         <div className="relative">
           <img
-            src={user?.profileImageUrl || ""}
+            src={user?.profileImageUrl || "https://imgur.com/a/LzzEvNI"}
             alt="Profile Image"
             className="w-20 h-20 bg-slate-200 rounded-full object-contain"
           />
