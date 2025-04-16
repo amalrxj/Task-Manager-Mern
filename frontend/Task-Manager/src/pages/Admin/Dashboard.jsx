d110c06d1ed7e2e2faced415bcc2c9bcf9b17c82
Amal Raj <amalrajkvdev@gmail.com>
2025-04-16 10:54:31 +0530

    gg

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useContext } from "react";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import { UserContext } from "../../context/userContextInstance";
